6cd95e92416cddb7deb3d4faac2cf739e7b342e0
dakling <dario.klingenberg@web.de>
2019-02-02 14:51:27 +0100

    nvimshell

---
+++ nvimshell/nvimshell.sh
@@ -0,0 +1,20 @@
+cd Documents/programming/lisp
+nvim-termite tutorial.lisp
+
+ssh klingenberg@klingenbergServer
+
+cd .dotfiles/dotfiles/binCustom/
+nvim-termite ./.dotfiles/dotfiles/binCustom/nterm
+nvim-termite ../INSTALL
+ls
+chmod +x nterm
+cd ..
+./INSTALL
+xmonad --restart
+man nvim
+
+nvim-termite nvimshellrc.vim
+nvim-termite .binlist
+
+git status
+git add nterm

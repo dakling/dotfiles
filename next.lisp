30bb227adabaf2678fae8c42829b5a55e2b99ecf
dakling <dario.klingenberg@web.de>
2019-07-19 13:40:38 +0200

    next

--- next.lisp
+++ next.lisp
@@ -5,4 +5,13 @@
 ;; Use development platform port.
 (setf (get-default 'port 'path)
       (format nil "~a/common-lisp/next/ports/gtk-webkit/next-gtk-webkit"
-              (uiop:getenv "HOME")))
+	      (uiop:getenv "HOME")))
+
+;; Donwload youtube videos
+(define-command youtube-dl-current-page ()
+  "Download a Youtube video in the currently open buffer."
+  (with-result (url (buffer-get-url))
+    (uiop:run-program
+     (list "youtube-dl" url))))
+
+;; (define-key *global-map* (key ". d") 'youtube-dl-current-page)

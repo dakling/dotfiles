3a82cbb1fafaa1eff9b4d7c35d016d71af4ad52a
Dario <dario.klingenberg@web.de>
2026-04-10 18:53:08 +0200

    add smart-splits

---
+++ nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,34 @@
+return {
+	"mrjones2014/smart-splits.nvim",
+	build = "./kitty/install-kittens.bash",
+	keys = {
+		{
+			"<A-h>",
+			function()
+				require("smart-splits").move_cursor_left()
+			end,
+			desc = "Move to left window",
+		},
+		{
+			"<A-l>",
+			function()
+				require("smart-splits").move_cursor_right()
+			end,
+			desc = "Move to right window",
+		},
+		{
+			"<A-j>",
+			function()
+				require("smart-splits").move_cursor_down()
+			end,
+			desc = "Move to below window",
+		},
+		{
+			"<A-k>",
+			function()
+				require("smart-splits").move_cursor_up()
+			end,
+			desc = "Move to above window",
+		},
+	},
+}
